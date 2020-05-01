f32c952035b0c509cefffd1512ada177f7d0ccfe
nnaemeka-git <64613292+nnaemeka-git@users.noreply.github.com>
2020-05-01 12:47:30 -0500

    KNN with Rusell Rao Distance metric for K in odd range of 1 to 15

---
+++ Rusell_Rao_similarity.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 11:44:35 2020
+
+@author: newma
+"""
+import time
+#import random
+import numpy as np
+import operator
+
+start_time = time.time()
+dset = np.loadtxt(r"C:\Users\newma\OneDrive\Desktop\nb_score.csv",delimiter=",",skiprows=1)
+
+#To view the dataset dimension
+dset.shape
+
+#To subset the dataset to only 100 records
+dset=dset[0:100,:]
+
+#Partition the score int 90- and 90+
+for i in range(len(dset)):
+    for j in range(986):
+        if dset[i][985] <90:
+            dset[i][985]= -90
+        else:
+            dset[i][985]=90
+
+N = len(dset)
+print(N)
+count=0
+count2 = 0
+for i in range(N):
+    if dset[i][985]<90:
+        count += 1
+    else:
+        count2 += 1
+print('Score less than 90 =',count)
+print('Score greater than 90 =',count2)
+
+#Rusell_Rao
+def Rusell_Rao(row1,row2):
+    m11=0
+    for i in range(len(row1)):
+        if row1[i]==1 and row2[i]==1:
+            m11 += 1
+        else:
+            pass
+    
+    a = m11
+    b = len(row1)
+    if a==0:
+        rao_dist = 0
+    else:
+        rao_dist = a/b
+    return rao_dist
+
+#Get the neighbors
+def get_neighbor(train,test,k):
+    dist_bucket=[]
+    dist_container=[]
+    
+    for rows in train:
+        dist = Rusell_Rao(test,rows)
+        dist_bucket.append((rows,dist))
+    #dist_container.sort(key=lambda tup:tup[1])
+    #dist_container=sort(key=operator.itemgetter(1),reverse=True)
+    #reverse=Trues makes the data to be sorted from highest to lowest    
+    dist_container=sorted(dist_bucket,key=operator.itemgetter(1),reverse=True)
+    
+    nebo=[]
+    for i in range(1,k+1):
+        nebo.append(dist_container[i][0]) #saves the first k number of rows 
+    return nebo
+
+#Make Prediction
+def my_prediction(nebos):
+    clv = []
+    for x in range(len(nebos)):
+        res= nebos[x][-1] #extracts the last column of each row (label)
+        clv.append(res)   #saves the label in a list
+        #print(clv)
+    return max(set(clv), key = clv.count) #returns the element that occured more frequently in clv list
+
+#Calculate accuracy percentage
+"""def accuracy_metric(actual, predicted):
+    match = 0    
+    for i in range(len(actual)):
+        if actual[i][-1] == predicted[i]:
+            match += 1
+    return (match / float(len(actual))) * 100.0"""
+
+#Implement Confusion Matrix
+def confusion_matrix(actual,predicted):
+    tp=0
+    fp=0
+    tn=0
+    fn=0
+    fs=0
+    
+    for i in range(len(actual)):
+        if actual[i][-1] == 90.0 and predicted[i]== 90.0:
+            tp += 1
+        elif actual[i][-1] == -90.0 and predicted[i]== 90.0:
+            fp += 1
+        elif actual[i][-1] == -90.0 and predicted[i]== -90.0:
+            tn += 1
+        elif actual[i][-1] == 90.0 and predicted[i]== -90.0:
+            fn += 1
+        else:
+            fs += 1
+            
+    a=(tp + tn)
+    b=(tp+tn+fp+fn)
+    if a==0 or b==0:
+        accu=0
+    else:
+        accu=a/b
+    
+    c=tp
+    d= (tp+fp)       
+    if c==0 or d==0:
+        precisi=0
+    else:
+        precisi=c/d
+    
+    e=(tp+fn)
+    if c==0 or e==0:
+        recal=0
+    else:
+        recal=c/e
+        
+    f=2*(precisi*recal)
+    g=(precisi+recal)
+    
+    if f==0 or g==0:
+        f_scor=0
+    else:
+        f_scor=f/g
+    
+    print('TP:',tp)
+    print('FP:',fp)
+    print('TN:',tn)
+    print('FN:',fn)
+    print('Missing:',fs)
+    print('Accuracy: ',accu*100)
+    print('Precision: ',precisi*100)
+    print('Recall: ',recal*100)
+    print('F Score: ',f_scor*100)
+    
+
+def main(i):
+    test_set=dset
+    pred=[]
+    print('K = ',i)
+    for x in range(len(test_set)):
+        neb=get_neighbor(dset,test_set[x],i)
+        res=my_prediction(neb)
+        pred.append(res)
+        print('Expected {}, Predicted {}'.format(test_set[x][-1],res))
+    #accuracy=accuracy_metric(test_set, pred)
+    confusion_matrix(test_set,pred)
+    #print('Accuracy 2:',accuracy,'percent for k=',i)
+    print('\n')
+        
+        
+
+for i in range(1,16,2):
+    main(i)
